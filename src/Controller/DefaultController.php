e2735f01db9cb1f439ae43b553251687edba06e1
Flowalt <ftcflowalt@gmail.com>
2020-03-05 14:58:03 +0100

    first commit

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+
+class DefaultController extends AbstractController {
+
+    function index(){
+        return $this->render('index.html.twig');
+    }
+
+
+}
